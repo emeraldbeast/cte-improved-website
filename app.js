92e65257fd73e2138aec8384285abf160a9ed33c
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-12-19 18:10:18 +0530

    All done, now website is live

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(process.cwd(), 'public')));
-
 app.set('views', path.join(process.cwd(), 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+if (!process.env.VERCEL) {
+  const port = process.env.PORT || 3000;
+  app.listen(port);
+}
 
 module.exports = app;
