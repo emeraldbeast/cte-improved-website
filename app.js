6fe8e9bdc2078372fcd3de6ba9bb57cf385776ee
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-12-19 17:58:30 +0530

    Trying once again for a deployment

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = process.env.port||3000;
 
 app.use(express.static('public'));
 app.set("view engine", "ejs");
@@ -9,4 +8,4 @@ app.get('/',(req,res)=>{
     res.render("index"); 
 })
 
-app.listen(port);
+module.exports = app; 
